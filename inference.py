df2c0d05021a68d8a880ef81d567f09cda3923f7
Rich0721 <rich101513@gmail.com>
2021-01-19 13:36:18 +0800

    s

--- inference.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras import models
-from utils.yolov4 import Yolov4
-
-model = Yolov4(weight_path="./logs/yolov4-39.h5")
-model.predict("../datasets/voc/JPEGImages/00167.jpg", random_color=False)
